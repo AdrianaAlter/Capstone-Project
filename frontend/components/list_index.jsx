c5a55352097dc1d8cbe63580a62704625214975c
AdrianaAlter <adriana.alter@gmail.com>
2016-04-11 12:26:22 -0400

    Update list_index.jsx

--- frontend/components/list_index.jsx
+++ frontend/components/list_index.jsx
@@ -1,32 +1,10 @@
 var React = require('react');
-// var ListStore = require('../store/list_store.js');
-// var ListActions = require('../actions/list_actions.js');
 var ListIndexItem = require('./list_index_item.jsx');
 var NewListButton = require('./new_list_button.jsx');
 var ApiUtil = require('../util/api_util.js');
 
 var ListIndex = React.createClass({
-  // getInitialState: function () {
-  //   return { lists: this.getStateFromStore() };
-  // },
-  //
-  // getStateFromStore: function () {
-  //   ApiUtil.fetchAllLists(this.props.boardId);
-  // },
-  //
-  // setNewState: function () {
-  //     this.setState( { lists: this.getStateFromStore() });
-  // },
-  //
-  // componentDidMount: function () {
-  //   this.listener = BoardStore.addListener(this.setNewState);
-  //   ApiUtil.fetchAllLists(this.props.boardId);
-  // },
-  //
-  // componentWillUnmount: function () {
-  //   this.listener.remove();
-  // },
-  //
+ 
   render: function () {
 
     if (!this.props.lists) {
