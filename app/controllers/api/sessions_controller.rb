b2f52fcf0d113c9b08b34dd57fd3b90ff6e011a8
AdrianaAlter <adriana.alter@gmail.com>
2016-04-11 12:15:41 -0400

    Update sessions_controller.rb

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,31 +1,19 @@
 class Api::SessionsController < ApplicationController
 
-  # def new
-  #   render :new
-  # end
 
   def create
 
-    # if params[:user][:email].length >= 1
-    #   @user = User.find_by_email(
-    #     params[:user][:email],
-    #     params[:user][:password]
-    #   )
-    # else
-
+   
       @user = User.find_by_name(
         params[:name],
         params[:password]
       )
 
-    # end
 
 
     if @user
       log_in(@user)
       render json: @user
-    # else
-    #   render :new
     end
 
   end
